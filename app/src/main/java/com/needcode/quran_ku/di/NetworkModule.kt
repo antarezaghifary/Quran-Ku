677477fb042843115ba00277b2a5dd98df3a164a
azerid <antareza08@gmail.com>
2022-08-09 21:54:07 +0700

    feature add utility Navigation & Observer

---
+++ app/src/main/java/com/needcode/quran_ku/di/NetworkModule.kt
@@ -0,0 +1,2 @@
+package com.needcode.quran_ku.di
+
---
+++ app/src/main/java/com/needcode/quran_ku/utility/ObserverExt.kt
@@ -0,0 +1,20 @@
+package com.needcode.quran_ku.utility
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+
+fun Lifecycle.addObservers(vararg lifecycleObserver: LifecycleObserver) {
+    lifecycleObserver.forEach {
+        addObserver(it)
+    }
+}
+
+fun Fragment.addObservers(vararg lifecycleObserver: LifecycleObserver) {
+    viewLifecycleOwner.lifecycle.addObservers(*lifecycleObserver)
+}
+
+fun AppCompatActivity.addObservers(vararg lifecycleObserver: LifecycleObserver) {
+    lifecycle.addObservers(*lifecycleObserver)
+}
